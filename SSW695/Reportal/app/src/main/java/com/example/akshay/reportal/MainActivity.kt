8d70f8d572f6427f5109f5364079ced8510f8ce8
Akshay Sunderwani <akshya672222@gmail.com>
2017-09-21 13:51:34 -0400

    Added registration and settings screen

--- SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/MainActivity.kt
+++ SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.akshay.reportal
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.BaseAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonPost.setText("POST")
     }
+
 }
