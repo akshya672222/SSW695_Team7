484ff5a7bf4ef8400dd1f1b78a02d06236bd616f
Akshay Sunderwani <akshya672222@gmail.com>
2017-10-05 11:57:29 -0400

    Fixed crash and issue of recent reports

--- SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/activity_feeds.kt
+++ SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/activity_feeds.kt
@@ -1,20 +1,20 @@
 package com.example.akshay.reportal
 
+/**
+ * Created by AKSHAY on 05/10/17.
+ */
+
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.BaseAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
-
-class MainActivity : AppCompatActivity() {
-
-    //String [] names_Categories = {"Low", "Medium", "High"}
+class activity_feeds : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        buttonPost.setText("POST")
+        setContentView(R.layout.activity_feeds)
     }
 
 }
