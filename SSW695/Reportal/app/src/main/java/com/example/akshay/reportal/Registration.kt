f34d8ce2a24bb595a727ec5b72c5c5c577e7db41
Akshay Sunderwani <akshya672222@gmail.com>
2017-10-05 15:51:51 -0400

    Added first intent

--- SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/Registration.kt
+++ SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/Registration.kt
@@ -3,6 +3,7 @@ package com.example.akshay.reportal
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_registration.*
 
 
@@ -11,10 +12,12 @@ class Registration : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
-
+//        println("hello started the activity")
+        println("hello came here..")
         registerBtn.setOnClickListener {
-//            val intent = Intent(applicationContext, MainActivity.class)
-//            startActivity(intent)
+            println("hello")
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 
