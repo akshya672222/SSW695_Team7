b54c43988d0f3c4d29e7885d3fcca5ede46e5250
Akshay Sunderwani <akshya672222@gmail.com>
2017-10-05 14:36:06 -0400

    Updated main activity

--- SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/Registration.kt
+++ SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/Registration.kt
@@ -1,12 +1,21 @@
 package com.example.akshay.reportal
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_registration.*
+
 
 class Registration : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        registerBtn.setOnClickListener {
+//            val intent = Intent(applicationContext, MainActivity.class)
+//            startActivity(intent)
+        }
     }
+
 }
--- SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/activity_feeds.kt
+++ SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/activity_feeds.kt
@@ -8,7 +8,7 @@ import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.BaseAdapter
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_feeds.*
 
 class activity_feeds : AppCompatActivity() {
 
