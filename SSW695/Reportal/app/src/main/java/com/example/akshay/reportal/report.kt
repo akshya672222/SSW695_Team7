4cf03b1e0956e9edfb1d60c63f21e3dba4a8f478
Sonali <sonalipatil1729@gmail.com>
2017-10-05 11:47:11 -0400

    adding report

---
+++ SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/report.kt
@@ -0,0 +1,16 @@
+package com.example.akshay.reportal
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by sonal on 10/5/2017.
+ */
+
+class report : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.report)
+    }
+}
