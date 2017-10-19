5f0bdc02b47941656e38e694d33d3850a32d27a6
pdbure15 <pranay.bure@gmail.com>
2017-10-19 17:25:15 -0400

    Added settings screen.

--- SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/Settings.kt
+++ SSW695/Reportal/app/src/main/java/com/example/akshay/reportal/Settings.kt
@@ -1,12 +1,30 @@
 package com.example.akshay.reportal
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class Settings : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        txtUserName.setOnClickListener {
+            val intent = Intent(this, Homepage::class.java)
+            ptuserName.visibility = View.VISIBLE
+        }
+
+        txtPassword.setOnClickListener {
+            val intent = Intent(this, Homepage::class.java)
+            ptPassword.visibility = View.VISIBLE
+        }
+
+        btnLogout.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
 }
