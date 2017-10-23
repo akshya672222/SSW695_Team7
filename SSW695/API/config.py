b90f0edc8df45298962fc2240d7287f24a63d636
pdbure15 <pranay.bure@gmail.com>
2017-10-23 13:41:39 -0400

    Added config.py to API.

---
+++ SSW695/API/config.py
@@ -0,0 +1,8 @@
+# This file contains all DB related settings
+# SQLALCHEMY is used to connect to Sqlite3 database
+DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///C:/Stevens data/3rd SEM/SSW_Capstone/Reportal_Flask API/Reportal.db'
+
+SECRET_KEY = 'thisisasecret'
+SQLALCHEMY_ECHO = True
